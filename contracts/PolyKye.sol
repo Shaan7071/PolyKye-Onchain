2f75c7315f640038ba143884638c76b8897ac7b8
Ishaan Banwait <i.banwait7@gmail.com>
2025-05-16 21:35:43 -0400

    Make contract upgradeable

--- contracts/PolyKye.sol
+++ contracts/PolyKye.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-contract PolyKye {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+contract PolyKye is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     // Events
     event TargetSubmitted(address indexed user, uint indexed targetId, string target);
     event ResultSubmitted(uint indexed targetId, string ligandSmiles, string ipfsHash);
-
+ 
     // Structures
     struct TargetSubmission {
         address user;
@@ -13,44 +15,38 @@ contract PolyKye {
         uint timestamp;
         bool processed;
     }
-
+ 
     struct Result {
         string ligandSMILES;       // Just the SMILES string
         string ipfsHash;     // All other data stored in IPFS
         uint timestamp;
     }
-
+ 
     // State Variables
     mapping(uint => TargetSubmission) public targets;
     mapping(uint => Result) public results;
-    address public owner;
     uint public targetCount;
     uint256 public submissionFee;
-
-    // Constructor to set values of state variables
-    constructor(uint256 _initialFee) {
-        submissionFee = _initialFee;
-        owner = msg.sender;
-    }
-
-    // Modifier to only allow the owner of this contract to perform certain actions
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
-        _;
-    }
-
+ 
+    function initialize(uint256 _initialFee) public initializer {
+    __Ownable_init(msg.sender);
+    __UUPSUpgradeable_init();
+    submissionFee = _initialFee;
+}
+ 
+ 
     // Owner function to update the fee
     function updateSubmissionFee(uint256 _newFee) public onlyOwner {
         submissionFee = _newFee;
     }
-
+ 
     // Owner function to allow withdrawal of collected fees
     function withdrawFees() public onlyOwner {
         uint256 balance = address(this).balance;
         require(balance > 0, "No fees to withdraw");
-        payable(owner).transfer(balance);
+        payable(owner()).transfer(balance);
     }
-
+ 
     // submitTarget function with paywall
     function submitTarget(string memory target) public payable returns (uint targetId) {
         // Check if the sent amount is sufficient
@@ -65,7 +61,7 @@ contract PolyKye {
             payable(msg.sender).transfer(excess);
         }
     }
-
+ 
      // Submit a result after off-chain computation
     function submitResult(
         uint targetId,
@@ -74,18 +70,18 @@ contract PolyKye {
     ) public onlyOwner{
         require(targetId < targetCount, "Invalid targetId");
         require(!targets[targetId].processed, "Already processed");
-
+ 
         results[targetId] = Result({
             ligandSMILES: smiles,
             ipfsHash: ipfsHash,
             timestamp: block.timestamp
         });
-
+ 
         targets[targetId].processed = true;
-
+ 
         emit ResultSubmitted(targetId, smiles, ipfsHash);
     }
-
+ 
     // Retrieve result data for a target
     function getResult(uint targetId) public view returns (
         string memory ligandSMILES,
@@ -99,4 +95,6 @@ contract PolyKye {
             result.timestamp
         );
     }
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+ 
 }
